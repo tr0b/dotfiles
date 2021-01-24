3eebb43d7eae386c7bacd873e7b59f77a135f224
tr0b <trobin0000@gmail.com>
2021-01-24 16:04:04 -0600

    Neovim Config - Fixed Lua file problem Changes and Additional Notes: - Symlink must be created in order to avoid compatibility issues - Symlink lsp config lua to .config nvim dir

---
+++ linux/neovim/lsp_config.lua
@@ -0,0 +1,57 @@
+ local lspconfig = require('lspconfig')
+ local on_attach = function(_, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    --require'diagnostic'.on_attach()
+    require'completion'.on_attach()
+ 
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>K', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', opts)
+ 
+ 
+  end
+ 
+  local servers = {'clangd', 'jsonls', 'tsserver', 'vimls', 'pyls', 'bashls'}
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+    }
+    --require'lspconfig'.julials.setup{}
+    --html LSP server config
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    require'lspconfig'.html.setup{
+
+    capabilities = capabilities,
+    on_attach = on_attach,
+
+    }
+    require'lspconfig'.solargraph.setup{
+	    cmd = { "solargraph", "stdio" },
+	    filetypes = { "ruby" },
+	    on_attach = on_attach
+    }
+	require'lspconfig'.hie.setup{
+		init_options = {
+			languageServerHaskell = {
+			    hlintOn = true;
+			    maxNumberOfProblems = 100;
+			    liquidOn = true;
+			    completionSnippetsOn = true;
+			    formatOnImportOn = true;
+			    formattingProvider = "brittany";
+			    languageServerVariant = "haskell-language-server"
+			  }
+		},
+		cmd = { "hie-wrapper", "--lsp" },
+		filetypes = { "haskell" }
+	}
+  end
--- mac/neovim/init.vim
+++ mac/neovim/init.vim
@@ -1,9 +1,9 @@
-source $HOME/.config/nvim/init_plugins.vim " <- initialize plugins
-source $HOME/.config/nvim/mapped.vim " <- initialize mapping settings
-source $HOME/.config/nvim/general.vim " <- initialize general vim settings
-source $HOME/.config/nvim/aesthetics.vim " <- initialize aesthetics
-lua require("lsp_config") " <- initialize lsp settings [LUA] 
-source $HOME/.config/nvim/lsp_config.vim " <- initialize lsp settings [VIM]
+source $HOME/.config/dotfiles/mac/neovim/init_plugins.vim " <- initialize plugins
+source $HOME/.config/dotfiles/mac/neovim/mapped.vim " <- initialize mapping settings
+source $HOME/.config/dotfiles/mac/neovim/general.vim " <- initialize general vim settings
+source $HOME/.config/dotfiles/mac/neovim/aesthetics.vim " <- initialize aesthetics
+lua require("lsp_config")
+source $HOME/.config/dotfiles/mac/neovim/lsp_config.vim " <- initialize lsp settings [VIM]
 " configure treesitter
 lua << EOF
 require'nvim-treesitter.configs'.setup {
