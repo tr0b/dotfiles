dd89970b2e55c4e458eda616593939f5861bd6ea
tr0b <trobin0000@gmail.com>
2021-06-21 09:11:19 -0600

    Added require declarations to general init lua file

--- linux/neovim/init.lua
+++ linux/neovim/init.lua
@@ -1,21 +1,4 @@
-vim.o.smarttab = true -- Smart Tab
-vim.o.expandtab = true -- Expand tab to spaces (use spaces on tab)
-vim.o.cursorline = true -- Set cursorline in file
-vim.o.autoindent = true -- Auto Indent
-vim.o.smartindent = true -- Smart Indent
-vim.o.clipboard = 'unnamed,unnamedplus' -- System clipboard used
-vim.o.hidden = true -- Hide buffers
-vim.o.syntax = 'true' -- Syntax enable
-vim.o.wildmenu = true -- Wildmenu
-vim.o.path = vim.o.path .. '**' -- path+=**
-vim.wo.number = true -- Set Number
-vim.wo.relativenumber = true -- Set Relative Number
-vim.wo.wrap = false -- no wrap lines
-vim.o.termguicolors = true -- termguicolors (enables 256 colour support in terminals)
-vim.o.t_Co = '256' -- 256 colours
-vim.o.inccommand = 'nosplit' -- live preview search & replace
-vim.o.ignorecase = true -- Ignores case in search
-vim.o.smartcase = true -- Applies smartcase
-vim.o.undofile = true -- Permanent undofile
---[[ vim.api.nvim_set_var('python3_host_prog', '/usr/bin/python3.9') ]]
---[[ vim.api.nvim_set_var('$GIT_EDITOR', 'nvr --remote-wait') ]]
+require('plugins') -- Plugins
+require('settings') -- General settings
+require('aesthetics') -- Aesthetic configuration
+require('keybindings') -- Keybindings, mapped keys & such
