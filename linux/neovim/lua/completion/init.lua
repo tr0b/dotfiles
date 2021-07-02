ebf8d50b6483eed618c5ea6ca3e00985fa7cb42f
tr0b <trobin0000@gmail.com>
2021-07-01 19:21:12 -0600

    Added compe auto completion capabilities to nvim lua configuration Changes Include: - Added keymappings for compe neovim (complete, confirm) - Added formating, detail, documentation, snippet support capabilities   to LSP servers - Added wildmenu, wildignorecase settings

--- linux/neovim/lua/completion/init.lua
+++ linux/neovim/lua/completion/init.lua
@@ -56,3 +56,5 @@ vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<C-Space", "compe#complete()", {silent = true, expr = true}) -- Complete text
+vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm('<CR>')", {silent = true, expr = true}) -- confirm snippet
