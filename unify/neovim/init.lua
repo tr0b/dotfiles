b0f9084551b37de8a6d6b8e3894bf9bd962a4be9
tr0b <60306480+tr0b@users.noreply.github.com>
2022-08-13 21:53:44 -0600

    Added new neorg config and penvim config files

--- unify/neovim/init.lua
+++ unify/neovim/init.lua
@@ -7,6 +7,8 @@ require('lsp') -- LSP
 require('completion') -- Auto-completion
 require('autopairs') -- Auto pairs settings
 require('twilight-config') -- Twilight dim code sections
--- require('zenmode-config') -- Twilight dim code sections
+require('zenmode-config') -- Twilight dim code sections
 require('indent-blankline-config') -- vertical lines config
-require('gitsigns-config') -- vertical lines config
+require('gitsigns-config') -- git signs config
+require('neorg-config') -- work in neorg config
+require('penvim-config') -- additional indent configs
