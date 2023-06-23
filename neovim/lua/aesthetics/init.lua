525c53e8abf2ae5fec2c9789f4c80347c0b0bcea
tr0b <trobinson3000600@gmail.com>
2023-06-23 17:38:18 -0600

    Will replace deadcolumn, fix some aesthetics

--- neovim/lua/aesthetics/init.lua
+++ neovim/lua/aesthetics/init.lua
@@ -1,24 +1,13 @@
+-- Regardless of colorscheme, highlight should have no bg, fg in gui
+-- vim.api.nvim_exec([[au ColorScheme * hi Normal ctermbg=none guibg=none]], false)
+-- Regardless of colorscheme, lines should have no bg, fg in gui
+-- vim.api.nvim_exec([[au ColorScheme * hi LineNr ctermbg=NONE guibg=NONE]], false)
 vim.o.background = "dark" -- Dark background, useful for transparency options
 vim.o.termguicolors = true -- termguicolors (256 colour)
-vim.o.colorcolumn = "79,119,159" -- 80 char limit for columns
-vim.o.conceallevel = 3 -- MARKDOWN conceal level
--- Regardless of colorscheme, highlight should have no bg, fg in gui
-vim.api.nvim_exec([[au ColorScheme * hi Normal ctermbg=none guibg=none]], false)
--- Regardless of colorscheme, lines should have no bg, fg in gui
-vim.api.nvim_exec([[au ColorScheme * hi LineNr ctermbg=NONE guibg=NONE]], false)
-vim.cmd.colorscheme("kanagawa-wave")
+vim.wo.colorcolumn = "79,119,159" -- 80 char limit for columns
+vim.cmd.colorscheme("kanagawa-dragon")
 
-vim.api.nvim_exec(
-	[[
-  call sign_define("LspDiagnosticsSignError", {"text" : "✗", "texthl" : "LspDiagnosticsError"})
-  call sign_define("LspDiagnosticsSignWarning", {"text" : "", "texthl" : "LspDiagnosticsWarning"})
-  call sign_define("LspDiagnosticsSignInformation", {"text" : "", "texthl" : "LspDiagnosticsInformation"})
-  call sign_define("LspDiagnosticsSignHint", {"text" : "", "texthl" : "LspDiagnosticsHint"})
-]],
-	true
-)
-
-local signs = { Error = "×", Warn = " ", Hint = "", Info = "¡" }
+local signs = { Error = "✗", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
@@ -251,3 +240,6 @@ require("toggleterm").setup({
 	direction = "float",
 	close_on_exit = true,
 })
+vim.api.nvim_set_hl(1, "Comment", { italic = true })
+require("colorizer").setup()
+require("virt-column").setup()
