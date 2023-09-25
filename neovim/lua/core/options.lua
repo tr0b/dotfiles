e0027af716bdc56aca93a998fc2be3f9dac570bd
tr0b <trobinson3000600@gmail.com>
2023-09-24 21:21:13 -0600

    Will add indent blank line options in core options.lua

--- neovim/lua/core/options.lua
+++ neovim/lua/core/options.lua
@@ -36,6 +36,7 @@ local opts = {
 	expandtab = true,
 	wrap = false,
 	termguicolors = true,
+	list = true,
 	number = true,
 	relativenumber = true,
 	timeout = true,
@@ -63,3 +64,12 @@ vim.cmd.colorscheme(colorscheme)
 vim.api.nvim_exec(neovim_terminal, false) -- Creates augroup through vimscript, sets GIT_EDITOR term
 vim.api.nvim_exec(undodir, false) -- Creates persistent undodir
 vim.api.nvim_set_hl(1, "Comment", { italic = true })
+vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
